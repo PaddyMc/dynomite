ed4f9acb365cebd56c70426899eea525a6b28f4d
PaddyMc <paddymchale@hotmail.com>
2017-07-11 19:22:23 +0100

    added email

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -4,9 +4,9 @@ from rest_framework import serializers
 class IDUserSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = IDUser
-		fields = ('url', 'user_id', 'first_name', 'last_name', 'password', 'date_of_birth', 'pictureURL')
+		fields = ('url', 'user_id', 'first_name', 'last_name', 'email', 'password', 'date_of_birth', 'pictureURL')
 
 class NewIDUserSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = NewIDUser
-		fields = ('url', 'user_id', 'first_name', 'last_name', 'password', 'date_of_birth', 'pictureURL')
+		fields = ('url', 'user_id', 'first_name', 'last_name', 'email', 'password', 'date_of_birth', 'pictureURL')
