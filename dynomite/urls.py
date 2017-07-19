285c02950b75023094e40219c8de50ec3ab11c36
PaddyMc <paddymchale@hotmail.com>
2017-07-19 22:10:45 +0100

    registernewuser

--- dynomite/urls.py
+++ dynomite/urls.py
@@ -8,6 +8,7 @@ from quickstart.models import IDUser, NewIDUser
 from quickstart import views
 from uploader import views as uploader_views
 from login import views as login_views
+from register_new_user import views as register_new_user_views
 
 from rest_framework import routers
 
@@ -22,4 +23,6 @@ urlpatterns = [
     url(r'^upload/(?P<filename>[^/]+)$', uploader_views.FileUploadView.as_view()),
     url(r'^hope/$', uploader_views.ListUsers.as_view()),
     url(r'^login/(?P<email>.+)/$', login_views.IDUserList.as_view()),
+    url(r'^registernewuser/', include('register_new_user.urls')),
+
 ] + static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
---
+++ register_new_user/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RegisterNewUserConfig(AppConfig):
+    name = 'register_new_user'
